47e3807fa3d54285d9710ca729b44872f6b1dd92
Le1ec0 <leandroizidoro@hotmail.com>
2023-01-20 21:59:44 -0300

    Corrected User Entity

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -4,6 +4,8 @@ namespace GradeHoraria.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public int? CursoId { get; set; }
+        public int? MateriaId { get; set; }
         public virtual ICollection<Cursos>? Cursos { get; set; }
         public virtual ICollection<Materias>? Materias { get; set; }
     }
