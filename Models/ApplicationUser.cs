a42300d5505e7c8a0632e9ca4100c51d896dd5cc
Le1ec0 <leandroizidoro@hotmail.com>
2023-01-23 14:23:37 -0300

    Added DisponibleDay for User

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -4,6 +4,7 @@ namespace GradeHoraria.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public int? DiaDisponivel { get; set; }
         public virtual ICollection<Cursos>? Cursos { get; set; }
         public virtual ICollection<Materias>? Materias { get; set; }
     }
