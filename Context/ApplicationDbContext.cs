a193d6e53209901dde0b0fc45b3dbfdaab301750
Le1ec0 <leandroizidoro@hotmail.com>
2023-01-28 01:29:34 -0300

    Corrected relationships

--- Context/ApplicationDbContext.cs
+++ Context/ApplicationDbContext.cs
@@ -10,7 +10,6 @@ namespace GradeHoraria.Context
         public DbSet<Curso> Cursos { get; set; }
         public DbSet<Periodo> Periodos { get; set; }
         public DbSet<Materia> Materias { get; set; }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Curso>()
@@ -25,11 +24,42 @@ namespace GradeHoraria.Context
                 .HasForeignKey(m => m.PeriodoId)
                 .OnDelete(DeleteBehavior.NoAction);
 
+            modelBuilder.Entity<Periodo>()
+                .HasOne(p => p.Curso)
+                .WithMany(c => c.Periodos)
+                .HasForeignKey(p => p.CursoId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Materia>()
+                .HasOne(m => m.Periodo)
+                .WithMany(c => c.Materias)
+                .HasForeignKey(m => m.PeriodoId)
+                .OnDelete(DeleteBehavior.NoAction);
+
             modelBuilder.Entity<Materia>()
                 .HasOne(m => m.Curso)
                 .WithMany(c => c.Materias)
                 .HasForeignKey(m => m.CursoId)
                 .OnDelete(DeleteBehavior.NoAction);
+        }
+        public override int SaveChanges()
+        {
+            var addedCursoEntities = ChangeTracker.Entries<Curso>()
+                .Where(e => e.State == EntityState.Added)
+                .Select(e => e.Entity);
+
+            foreach (var addedCurso in addedCursoEntities)
+            {
+                var periodo = new Periodo
+                {
+                    CursoId = addedCurso.Id,
+                    Curso = addedCurso,
+                    Id = addedCurso.Periodo
+                };
+                Periodos.Add(periodo);
+            }
+
+            return base.SaveChanges();
         }
     }
 }
