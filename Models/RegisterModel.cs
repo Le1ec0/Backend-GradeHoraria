0d92557af65bf68c4f1d4450f24545b2e303f382
Le1ec0 <leandroizidoro@hotmail.com>
2023-01-31 15:56:02 -0300

    Corrected RegisterModel

--- Models/RegisterModel.cs
+++ Models/RegisterModel.cs
@@ -6,19 +6,15 @@ namespace GradeHoraria.Models
     public class RegisterModel
     {
         [Required(ErrorMessage = "Insira o nome do usuário.")]
-        [Description("Insira o nome do usuário.")]
         public string? displayName { get; set; }
 
         [Required(ErrorMessage = "Insira o apelido do usuário.")]
-        [Description("Insira o apelido do usuário.")]
         public string? mailNickname { get; set; }
 
         [Required(ErrorMessage = "Insira o e-mail do usuário.")]
-        [Description("Insira o e-mail do usuário.")]
         public string? userPrincipalName { get; set; }
 
         [Required(ErrorMessage = "Insira a senha do usuário.")]
-        [Description("Insira a senha do usuário.")]
         public string? password { get; set; }
     }
 }
