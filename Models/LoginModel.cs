6fa93948ca25f19e3df3d633cd94df2edd92e373
Le1ec0 <leandroizidoro@hotmail.com>
2023-02-01 18:50:30 -0300

    Corrected LoginModel class

--- Models/LoginModel.cs
+++ Models/LoginModel.cs
@@ -4,10 +4,10 @@ namespace GradeHoraria.Models
 {
     public class LoginModel
     {
-        [Required(ErrorMessage = "User Name is required")]
+        [Required(ErrorMessage = "E-mail é necessário.")]
         public string? Username { get; set; }
 
-        [Required(ErrorMessage = "Password is required")]
+        [Required(ErrorMessage = "Senha é necessária.")]
         public string? Password { get; set; }
     }
 }
