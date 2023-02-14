990f9edced48eda1692997b240f9a0aedc8a4172
Le1ec0 <leandroizidoro@hotmail.com>
2023-02-14 20:21:46 -0300

    Saved CreateRoles method

--- Models/UserRoles.cs
+++ Models/UserRoles.cs
@@ -9,3 +9,28 @@ namespace GradeHoraria.Models
         public const string Usuario = "Usuário";
     }
 }
+
+/*private async Task CreateRoles()
+    {
+        // Check if the role already exists
+        if (!await _roleManager.RoleExistsAsync(UserRoles.AdminMaster))
+        {
+            await _roleManager.CreateAsync(new IdentityRole(UserRoles.AdminMaster));
+        }
+        if (!await _roleManager.RoleExistsAsync(UserRoles.Admin))
+        {
+            await _roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
+        }
+        if (!await _roleManager.RoleExistsAsync(UserRoles.Coordenador))
+        {
+            await _roleManager.CreateAsync(new IdentityRole(UserRoles.Coordenador));
+        }
+        if (!await _roleManager.RoleExistsAsync(UserRoles.Professor))
+        {
+            await _roleManager.CreateAsync(new IdentityRole(UserRoles.Professor));
+        }
+        if (!await _roleManager.RoleExistsAsync(UserRoles.Usuario))
+        {
+            await _roleManager.CreateAsync(new IdentityRole(UserRoles.Usuario));
+        }
+    }*/
