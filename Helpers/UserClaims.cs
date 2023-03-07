3068c76b49d07cde2e832f7e9bc3fb1b9a060df0
Le1ec0 <leandroizidoro@hotmail.com>
2023-03-07 19:23:39 -0300

    Created UserClaims helper class

---
+++ Helpers/UserClaims.cs
@@ -0,0 +1,12 @@
+
+
+namespace GradeHoraria.Helpers
+{
+    public class UserClaims
+    {
+        public string Sub { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        // add other properties as needed
+    }
+}
